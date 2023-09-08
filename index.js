2656dd7da67a6497a1216471900d5fed62689de1
PrabhuM143 <prabhumurthy143@gmail.com>
2023-09-08 15:47:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000; // You can change the port as needed
+
+app.use(bodyParser.json());
+
+app.post("/bfhl", (req, res) => {
+  try {
+    const inputData = req.body.data;
+
+    // Separate numbers and alphabets
+    const numbers = inputData.filter((item) => !isNaN(item));
+    const alphabets = inputData.filter(
+      (item) => isNaN(item) && /^[a-zA-Z]$/.test(item)
+    );
+
+    // Find the highest alphabet
+    const highestAlphabet =
+      alphabets.length > 0
+        ? String.fromCharCode(
+            Math.max(...alphabets.map((char) => char.charCodeAt(0)))
+          )
+        : "";
+
+    const response = {
+      is_success: true,
+      user_id: "john_doe_17091999",
+      email: "john@xyz.com",
+      roll_number: "ABCD123",
+      numbers: numbers,
+      alphabets: alphabets,
+      highest_alphabet: [highestAlphabet],
+    };
+
+    res.json(response);
+  } catch (err) {
+    res.send({
+      is_success: false,
+    });
+  }
+});
+
+app.get("/bfhl", (req, res) => {
+  res.send({
+    operation_code: 1,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
